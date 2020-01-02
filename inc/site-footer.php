af6a43d0903ad606b44cd7b24e1a4669f6c201df
Mark Corpuz <social@markcorpuz.com>
2020-01-01 16:12:41 -0800

    Added site footer credits and footer widgets to replace existing layouts

--- inc/site-footer.php
+++ inc/site-footer.php
@@ -9,16 +9,22 @@
 **/
 
 /**
- * Site Footer
- *
+ * SETUP-STARTER
+ * 
+ * Site Footer w Credits SWP
+ * Add credits section inside the site footer
  */
-function ea_site_footer() {
-	echo '<div class="footer-left">';
-		echo '<p class="copyright">Copyright &copy; ' . date( 'Y' ) . ' ' . get_bloginfo( 'name' ) . '®. All Rights Reserved.</p>';
-		echo '<p class="footer-links"><a href="' . home_url( 'privacy-policy' ) . '">Privacy Policy</a> <a href="' . home_url( 'terms' ) . '">Terms</a></p>';
-		echo '<p class="cafemedia">An Elite Cafemedia Food Publisher</p>';
+function setup_site_footer_wcredits_swp() {
+	echo '<div class="credit">';
+		echo '<div class="sitefor">';
+			echo '<div class="credit-brand"></div>';
+			echo '<div class="credit-copyright">Copyright &copy; ' . date( 'Y' ) . ' ' . get_bloginfo( 'name' ) . '®. All Rights Reserved | <a href="' . home_url( 'privacy-policy' ) . '">Privacy Policy</a> <a href="' . home_url( 'terms' ) . '">Terms</a></div>';
+			echo '<div class="credit-designby">Site Design by <a href="https://smarterwebpackages.com/">SmarterWebPackages.com</a></div>';
+		echo '</div>';
+		echo '<div class="siteby">';
+			echo '<a href="https://smarterwebpackages.com/">SmarterWebPackages.com</a>';
+		echo '</div>';
 	echo '</div>';
-	echo '<a class="backtotop" href="#main-content">Back to top' . ea_icon( array( 'icon' => 'arrow-up' ) ) . '</a>';
 }
-add_action( 'genesis_footer', 'ea_site_footer' );
+add_action( 'genesis_footer', 'setup_site_footer_wcredits_swp' );
 remove_action( 'genesis_footer', 'genesis_do_footer' );
